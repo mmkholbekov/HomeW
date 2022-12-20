d58f6f4b60b7ced6d1a958ec965b554013c107ca
mmkholbekov <120006110+mmkholbekov@users.noreply.github.com>
2022-12-21 01:22:15 +0400

    hero.py

--- hero.py
+++ hero.py
@@ -22,11 +22,80 @@ class SuperHero:
     def __len__(self):
         return len(self.catchphrase)
 
-Hero = SuperHero('Hiro','Homada\n','Fly\n','30','a')
+Hero = SuperHero('Bug','M7055\n','Fly\n','30','Not')
 Hero.x()
 Hero.a()
 print(Hero)
 print(f'catchphrase: {len(Hero.catchphrase)}')
+
+
+# Hw2
+
+class Robot(SuperHero):
+    people = 'people'
+
+    def __init__(self, name,nickname,superpower,health_points,catchphrase, damage=False, fly=False):
+        super().__init__(name,nickname,superpower,health_points,catchphrase)
+        self.damage = damage
+        self.fly = fly
+    def n(self):
+        hd = self.health_points ** 2
+        self.health_points = hd
+    def a(self):
+        self.fly = True
+    def phrase(self):
+        print('fly in the True_phrase')
+
+hdr = Robot('Rob','OTO\n','Dexterity\n', 10, 'no')
+hdr.n()
+print(hdr)
+print(f'Damage:{hdr.damage}')
+hdr.a()
+print(f'Fly: {hdr.fly}')
+hdr.phrase()
+
+
+class Robot2(SuperHero):
+    people = 'people'
+
+    def __init__(self,name,nickname,superpower,health_points,catchphrase, damage=False, fly=False):
+        super().__init__(name,nickname,superpower,health_points,catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def n(self):
+        dest = self.health_points ** 2
+        self.health_points = dest
+
+    def a(self):
+        self.fly = True
+
+    def phrase(self):
+         print('fly in the True_phrase')
+
+des = Robot2('Dotty', 'Bot\n', 'Power\n', 10, 'No')
+des.n()
+print(des)
+print(f'Damage: {des.damage}')
+des.a()
+print(f'Fly: {des.fly}')
+des.phrase()
+
+class Villian(Robot2):
+    monster = 'monster'
+
+    def __init__(self, name,nickname,superpower,health_points,catchphrase):
+        super().__init__(name,nickname,superpower,health_points,catchphrase)
+    def gen_x(self):...
+    def crit(self):
+        print(f'Crit:{self * 2}')
+newf = Villian('Wall-E', 'R2D2\n', 'Intelegence\n', 50, 'robot')
+print(newf)
+newf.gen_x()
+Villian.crit(hdr.damage)
+
+
+
 
 
 
